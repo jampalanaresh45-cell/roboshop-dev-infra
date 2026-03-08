91a879f898cae8e34ad8b6e27c84f88a46e26424
Naresh Jampala <jampalanaresh45@gmail.com>
2026-03-08 15:30:58 +0530

    ansible

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 dnf install ansible -y
-ansible-pull -U https://github.com/jampalanaresh45-cell/ansible.git \
--e component=mongodb \
--e env=dev \
-ansible-roboshop-roles-tf-main/main.ymls/ansible-roboshop-roles-tf-main -e component=mongodb main.yaml
+ansible-pull -U https://github.com/jampalanaresh45-cell/ansible.git \ -e component=mongodb \ -e env=dev \ ansible-roboshop-roles-tf-main/main.yml
