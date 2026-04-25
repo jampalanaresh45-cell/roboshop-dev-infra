1d55ffa785b40447e9fad4795e5da292e1e5dae8
Naresh Jampala <jampalanaresh45@gmail.com>
2026-04-25 18:39:53 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 component=$1
+environment=$2
 dnf install ansible -y
 # ansible-pull -U https://github.com/jampalanaresh45-cell/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 
@@ -26,4 +27,4 @@ else
     cd $ANSIBLE_DIR
 fi
 
-ansible-playbook -e component=$component main.yaml
+ansible-playbook -e component=$component -e environment=$environment main.yaml
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -119,3 +119,48 @@ provisioner "remote-exec" {
   }
 }
 
+resource "aws_instance" "mysql" {
+    ami = local.ami_id
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [local.mysql_sg_id]
+    subnet_id = local.database_subnet_id
+    iam_instance_profile = aws_instance_profile.mysql.name
+
+    tags = merge(
+      local.common_tags,
+       {
+          Name = "${local.common_name_suffix}-mysql" ### roboshop-dev-mysql
+      }
+    )      
+}
+
+resource "aws_instance_profile" "mysql" {
+  name = "${local.common_name_suffix}-mysql-instance-profile"
+  role = "EC2SSMParameterRead" 
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id,
+  ]
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        password = "DevOps321"
+        host     = aws_instance.mysql.private_ip
+}
+
+provisioner "file" {
+  source = "bootstrap.sh"
+  destination = "/tmp/bootstrap.sh"
+}
+provisioner "remote-exec" {
+    inline = [
+        
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql dev"
+        # "sudo sh /tmp/bootstrap.sh mongodb"
+    ]
+  }
+}
+
