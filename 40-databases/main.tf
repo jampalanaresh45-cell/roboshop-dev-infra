bacfae8111b10976037560056d4eada28ca1dc10
Naresh Jampala <jampalanaresh45@gmail.com>
2026-04-25 18:49:43 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -124,7 +124,7 @@ resource "aws_instance" "mysql" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [local.mysql_sg_id]
     subnet_id = local.database_subnet_id
-    iam_instance_profile = aws_instance_profile.mysql.name
+    iam_instance_profile = aws_iam_instance_profile.mysql.name
 
     tags = merge(
       local.common_tags,
